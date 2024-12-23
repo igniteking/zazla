c63a5424458c425f3bae51fb7388e5dc02ec9b97
Micheal <khanzaidan786@gmail.com>
2024-12-23 13:48:26 +0530

    link

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 export default function Home() {
   return (
     <div className="flex flex-1 flex-col gap-4 p-4 pt-0">
+      {/* https://vuero.cssninja.io/sidebar/dashboards/banking-1 */}
     </div>
   );
 }
